c2ae2b0f2e875281f88f63e909ef83b6d66045ed
Ana-Dare <analauradare467@gmail.com>
2025-09-05 08:32:44 -0300

    feat: request api

--- src/componentes/SaudacaoUsuario/index.tsx
+++ src/componentes/SaudacaoUsuario/index.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { IUsuario } from "../../types";
+import { obterUsuario } from "../../api";
 
 export const StyledUsuario = styled.div`
   grid-area: usuario;
@@ -13,10 +16,21 @@ export const StyledUsuario = styled.div`
   }
 `;
 
-const SaudacaoUsuario = () => {
+  const SaudacaoUsuario = () => {
+    const [usuario, setusuario] = useState<IUsuario | null>(null);
+
+    const buscaUsuario = async () => {
+      const response = await obterUsuario();
+      setusuario(response[0]);
+    };
+
+    useEffect(() => {
+      buscaUsuario();
+    });
+
   return (
     <StyledUsuario>
-      <h1>Olá, Neilton</h1>
+      <h1>Olá, {usuario?.nome}</h1>
       <p>Veja como estão suas finanças hoje.</p>
     </StyledUsuario>
   );
